631821de6e3ee0e478007311d476d47b0652b408
kiyosss <kiyotakisota0000@gmail.com>
2026-09-08 20:54:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,9 +44,7 @@ print("COMMAND REGISTRATION STATUS:", response.status_code)
 print("COMMAND REGISTRATION RESPONSE:", response.text)
 
 def send_messages(application_id, interaction_token):
-    pass
-url = f"https://discord.com/api/v10/webhooks/{application_id}/{interaction_token}"
-
+    url = f"https://discord.com/api/v10/webhooks/{application_id}/{interaction_token}"
 response = requests.post(
     url,
     json={
